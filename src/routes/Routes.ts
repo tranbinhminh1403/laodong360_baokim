7dc026be3ba1618a50377459fe6fb4575ce9992a
tranbinhminh1403 <diobrando1403@gmail.com>
2025-02-20 16:37:55 +0700

    final: create customers and tickets contact center

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { handleCreateOrder } from '../controllers/OrderController';
 import { handleWebhookCallback } from '../controllers/WebhookController';
-import { testContactCenterLogin } from '../controllers/TestController';
 
 const router = Router();
 
 router.post('/send-order', handleCreateOrder);
 router.post('/webhook', handleWebhookCallback);
-router.get('/testlogin', testContactCenterLogin);
 
 export default router; 
