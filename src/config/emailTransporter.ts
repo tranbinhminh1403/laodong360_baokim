2c25e5c9d215e140a9a99e4a9750177aa363ef2a
tranbinhminh1403 <diobrando1403@gmail.com>
2025-02-26 17:29:29 +0700

    log mail

--- src/config/emailTransporter.ts
+++ src/config/emailTransporter.ts
@@ -1,14 +1,23 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const username = process.env.MAIL_USERNAME;
+const password = process.env.MAIL_PASSWORD;
+
+console.log(username + '\n' + password);
 
 export const transporter = nodemailer.createTransport({
+    // service: 'gmail',
     host: 'pro57.emailserver.vn',
     port: 465,
-    secure: true, // use SSL/TLS
-    pool: true,
-    maxConnections: 5,
+    secure: true,
+    pool: true, 
+    maxConnections: 5, 
     maxMessages: Infinity,
     auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD
+        user: username,
+        pass: password
     }
 }); 
